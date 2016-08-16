5105656dfbf094d091ba77bba3e7d2a58bcf5928
ewappel@gmail.com <ewappel@gmail.com>
2016-08-16 09:30:28 -0700

    simple percent match

---
+++ ETL Database Developer.py
@@ -0,0 +1,55 @@
+Description:
+
+As part of the IT Infrastructure Program, we are defining the future state of Enterprise 
+#Demand Forecasting and Infrastructure Planning
+ (EDFIP)
+, including
+# establishing effective demand forecasting, capacity planning and infrastructure planning processes. 
+The EDFIP team will develop the processes and data analytical methodologies required to establish
+ a holistic approach to enterprise infrastructure planning.
+  This position will participate in
+  the development and delivery of an IT infrastructure utilization data warehouse and
+   reporting system that will support infrastructure planning decision making processes.
+    #We require a highly experienced and results-driven IT database professional who is
+     #extremely collaborative and accustomed to working within a matrix organizational structure.
+      This role will
+  #     engage with capacity planning, demand forecasting and
+  #     infrastructure planning stakeholders to develop data requirements and 
+  #     implement data extract and transformation programs to establish a data warehouse
+  #      and reporting solution.
+
+Under the direction of the Lead Database Architect this role will be responsible for
+ 
+ #database extract, transform, cleansing and load (ETL) script development, ETL automation and report development.
+  He/she will also
+  # document data warehouse requirements and will participate in data modeling as required.
+   
+   He/she will 
+ #  gather and analyze infrastructure capacity and demand metrics and business data
+ #   from a variety of sources, and use it to recommend reporting solutions that support
+ #    infrastructure capacity and trend analysis.
+
+
+Top 3-5 Daily Responsibilities:
+
+Data Warehouse requirements gathering/documentation
+ETL development in SSIS
+#SQL stored procedure development
+Report development in Tableau or other BI tools
+Top 3-5 Required Skills:
+
+Experience developing ETL in SSIS or similar ETL tool
+Requirements documentation
+Report development, preferably in Tableau
+Advanced MS-SQL development
+MS-Office expert
+Desired Skills:
+
+Microsoft Certified DBA or MCSE
+Tableau Report Development
+Executive dashboard development
+Soft Skills:
+
+Strong written and oral communications
+Collaboration
+Facilitation
--- resume_scanner.py
+++ resume_scanner.py
@@ -1,46 +1,121 @@
 #!/usr/bin/python
 
-with open('resume.txt', 'r') as myfile:
-    data=myfile.read()
-print data
+# import resume.txt
+file=open("resume.txt","r+")
 
-# count the words in the resume
-#print "resume word count"
-#file=open("resume.txt","r+")
-#print file
-#wordcount={}
-#for word in file.read().split():
-#    if word not in wordcount:
-#        wordcount[word] = 1
-#    else:
-#        wordcount[word] += 1
-#print type(wordcount)
-
-
-#import operator
-#x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
-#sorted_wordcount = sorted(wordcount.items(), key=operator.itemgetter(1), reverse=True)
-
-#for k,v in sorted_wordcount:
-#    if v>=2:
-#    	print k, v
-#print sorted_wordcount
-
-print "job description word count"
-file=open("job_description.txt","r+")
+# create dict
 wordcount={}
 for word in file.read().split():
     if word not in wordcount:
         wordcount[word] = 1
     else:
         wordcount[word] += 1
-#print type(wordcount)
+
+# create list from dict
+temp = []
+for key, value in wordcount.iteritems():
+	#print key
+	temp.append(key)
+
+# lowercase
+temp = [item.lower() for item in temp]
+
+# remove punctuation
+import string
+punct = set(string.punctuation)
+for x in range(0,len(temp)):
+	punct_flag = 0
+ 	for y in range(0,len(punct)):
+ 		#print x, temp[x], y, list(punct)[y], temp[x].find(list(punct)[y])
+ 		if temp[x].find(list(punct)[y]) != -1:
+ 			punct_flag = 1
+ 			temp[x] = temp[x].replace(list(punct)[y],'')
+ 			#print punct_flag, temp[x]
+
+# remove duplicates
+temp = dict.fromkeys(temp).keys()
+# sort
+temp.sort();
+resume_text = temp
 
 
-import operator
-#x = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
-sorted_wordcount = sorted(wordcount.items(), key=operator.itemgetter(1), reverse=True)
 
-for k,v in sorted_wordcount:
-    if v>=2:
-    	print k, v
+
+
+
+
+
+
+
+
+# import resume.txt
+file=open("job.txt","r+")
+
+# create dict
+wordcount={}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+
+# create list from dict
+temp = []
+for key, value in wordcount.iteritems():
+	#print key
+	temp.append(key)
+
+# lowercase
+temp = [item.lower() for item in temp]
+
+# remove punctuation
+import string
+punct = set(string.punctuation)
+for x in range(0,len(temp)):
+	punct_flag = 0
+ 	for y in range(0,len(punct)):
+ 		#print x, temp[x], y, list(punct)[y], temp[x].find(list(punct)[y])
+ 		if temp[x].find(list(punct)[y]) != -1:
+ 			punct_flag = 1
+ 			temp[x] = temp[x].replace(list(punct)[y],'')
+ 			#print punct_flag, temp[x]
+
+# remove duplicates
+temp = dict.fromkeys(temp).keys()
+# sort
+temp.sort();
+job_text = temp
+#print job_text
+
+
+
+
+
+
+
+
+
+#Loop through the job decription
+matched_list = []
+unmatched_list=[]
+for x in range(0,len(job_text)):
+	match_flag = 0
+	for y in range(0,len(resume_text)):
+		
+		if job_text[x] == resume_text[y]:
+			match_flag = 1
+			matched_list.append(job_text[x])
+			#print x, b_sorted[x], y, a_sorted[y], match_flag
+		
+		#print y, (len(a_sorted)-1)
+		if y == (len(resume_text)-1):
+			if match_flag == 0:
+				unmatched_list.append(job_text[x])
+				#print b_sorted[x]
+
+print "Results:", "\n"
+print "matched:", matched_list, "\n"
+print "unmatched:", unmatched_list, "\n"
+
+job_word_count = len(job_text)
+print round((float(len(matched_list)) / float(job_word_count) * 100),1), "% match", "\n"
